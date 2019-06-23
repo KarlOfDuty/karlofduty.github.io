eaee26464b242b59b79a74eefefd07b0bd3716ad
Karl Essinger <xkaess22@gmail.com>
2019-06-23 02:19:15 +0200

    Removed dev info, added some text to loading screen

--- js/loading_screen.js
+++ js/loading_screen.js
@@ -4,6 +4,25 @@ var loadingBar = null;
 var currentFile = "";
 var currentStatus = "";
 
+loadingBar = new ProgressBar.Line("#loading-bar",
+    {
+        strokeWidth: 6,
+        easing: 'easeOut',
+        duration: 1400,
+        color: '#4a8af4',
+        trailColor: '#eee',
+        trailWidth: 2,
+        svgStyle: {width: '100%', height: '100%'},
+        from: {color: '#777777'},
+        to: {color: '#1194f0'},
+        step: (state, loadingBar) =>
+        {
+            $("#percentage").text(Math.round(loadingBar.value() * 100) + '%');
+        }
+    });
+
+loadingBar.animate(0.0049);
+
 // Ran when game info is sent
 function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode )
 {
@@ -22,23 +41,6 @@ function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamem
 function SetFilesTotal(total)
 {
     allFiles = total;
-
-    loadingBar = new ProgressBar.Line("#loading-bar",
-    {
-        strokeWidth: 5,
-        easing: 'easeOut',
-        duration: 1400,
-        color: '#4a8af4',
-        trailColor: '#eee',
-        trailWidth: 1,
-        svgStyle: {width: '100%', height: '100%'},
-        from: {color: '#777777'},
-        to: {color: '#1194f0'},
-        step: (state, loadingBar) =>
-        {
-            $("#percentage").text(Math.round(loadingBar.value() * 100) + '%');
-        }
-    });
 }
 
 function DownloadingFile(fileName)
@@ -64,8 +66,8 @@ function SetFilesNeeded(needed)
 }
 
 // Developer functions, should not be active in production
-GameDetails("server name", "karlofduty.com", "ttt_stargate_b3", 20, "STEAM:0:1:543355", "murder");
-SetFilesTotal(10);
-SetFilesNeeded(5);
-DownloadingFile("player_viewmodel_ttt.mod");
-SetStatusChanged("Loading resources...");
+//GameDetails("server name", "karlofduty.com", "ttt_stargate_b3", 20, "STEAM:0:1:543355", "murder");
+//SetFilesTotal(10);
+//SetFilesNeeded(5);
+//DownloadingFile("player_viewmodel_ttt.mod");
+//SetStatusChanged("Loading resources...");
